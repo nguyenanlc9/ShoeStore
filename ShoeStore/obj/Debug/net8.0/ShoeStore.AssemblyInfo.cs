a0302aaaa38390f3f071bc25b6747a0af75ab393
khac-thuong <tt4060779@gmail.com>
2025-01-06 13:12:29 +0700

    up home sale

--- ShoeStore/obj/Debug/net8.0/ShoeStore.AssemblyInfo.cs
+++ ShoeStore/obj/Debug/net8.0/ShoeStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048617cd263dccbcd3ae6a7dbc4859edd27f7a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7111692f4d0166a737307cebef9957fabb5b7c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
